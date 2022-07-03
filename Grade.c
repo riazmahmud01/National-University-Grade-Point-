857b93a9837932080452a14367cafcbca773b24b
riazmahmud01 <riazmahmudgd@gmail.com>
2022-07-03 11:49:58 +0600

    Full Marks Add

--- Grade.c
+++ Grade.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 { // Marks Section Input
-    int marks;
+    float marks;
     printf("Enter Your Exam Marks : ");
-    scanf("%d", &marks);
+    scanf("%f", &marks);
 
     // Grade Point Add
-    if (marks >= 80 && marks < 100)
+    if (marks >= 80 && marks <= 100)
     {
         printf("A+");
     }
@@ -14,8 +14,40 @@ int main()
     {
         printf("A");
     }
+    else if (marks >= 70 && marks < 75)
+    {
+        printf("A-");
+    }
     else if (marks >= 65 && marks < 70)
     {
         printf("B+");
+    }
+    else if (marks >= 60 && marks < 65)
+    {
+        printf("B");
+    }
+    else if (marks >= 55 && marks < 60)
+    {
+        printf("B-");
+    }
+    else if (marks >= 50 && marks < 55)
+    {
+        printf("C+");
+    }
+    else if (marks >= 45 && marks < 50)
+    {
+        printf("C");
+    }
+    else if (marks >= 40 && marks < 45)
+    {
+        printf("D");
+    }
+    else if (marks >= 0 && marks <= 39)
+    {
+        printf("Fail");
+    }
+    else
+    {
+        printf("Wrong Number");
     }
 }
